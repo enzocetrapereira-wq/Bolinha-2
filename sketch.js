6563eb3b8d93edb6cd23a22b51f4dde2c31ea380
enzocetrapereira-wq <enzo.cetra.pereira@escola.pr.gov.br>
2026-09-08 07:56:19 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,35 @@
+//Projeto Bolinha 02
+let yBolinha=200;
+let xBolinha=200;
+let raio = 15;
+let yVelocidade = 3;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  mostraBolinha();
+  movimentaBolinha();
+  verificaColisao();
+}
+
+
+function mostraBolinha(){
+   circle(xBolinha, yBolinha, raio)
+}
+
+
+//====Função movimenta Bolinha-----
+function movimentaBolinha(){
+yBolinha += yVelocidade
+}
+
+
+//-----Verifica Colisao-----
+function verificaColisao(){
+ if(yBolinha > height || yBolinha < 0){
+   yVelocidade *= -1;
+ }
+}
